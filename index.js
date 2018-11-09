ae69afdad2cd0c98a2301caa59b3cd6c9e1fbe39
unknown <brandongrych@gmail.com>
2018-11-09 11:42:48 -0600

    Post actions/projects added

--- index.js
+++ index.js
@@ -34,7 +34,43 @@ server.get('/api/actions', (req, res) => {
 
 
 // POST
-
+server.post('/api/actions', (req, res) => {
+    const action = {
+        "project_id": req.body.project_id,
+        "description": req.body.description,
+        "notes": req.body.notes
+    }
+        if(action.description.length > 128){
+        return res.status(413).json({error: "Description has too many characters."})
+    }
+        if(!action.project_id || !action.description || !action.notes){
+        return res.status(400).json({error: "New actions must contain an ID, a description, and notes."})
+    }
+        // does project exist? 
+    projectDb.get(action.project_id)
+    .then(reply => {
+        console.log(reply);
+        if(!reply){
+            return res.status(404).json({error: "No project with that ID was found."})
+        } else {
+            actionDb.insert(action)
+            .then(reply => {
+                console.log(reply);
+                return res.status(201).json("Action added.");
+            })
+            .catch(err => {
+                console.log(err);
+                return res.status(500).json({error: "Error adding action."})
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: `Error finding project ID ${action.project_id}`})
+    })
+    
+    
+})
 
 
 // DELETE
@@ -61,7 +97,27 @@ server.get('/api/projects', (req, res) => {
 
 
 // POST
-
+server.post('/api/projects', (req, res) => {
+    const project = {
+        "name": req.body.name,
+        "description": req.body.description
+    }
+        if(project.name.length > 128){
+        return res.status(413).json({error: "Name contains too many characters."})
+    }
+        if(project.name.length === 0 || project.description.length === 0){
+        return res.status(400).json({error: "Project must have name and description."})
+    }
+        projectDb.insert(project)
+    .then(reply => {
+        console.log(reply);
+        return res.status(201).json({message: "Project added."});
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: "Error adding project."})
+    })
+})
 
 
 // DELETE
